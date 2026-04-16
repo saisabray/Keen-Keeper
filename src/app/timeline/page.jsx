77e5df60dbd807b9e39888644aa7a66f16552538
saisabray <saisabray1402@gmail.com>
2026-04-16 09:22:36 +0600

    fix filter issue

--- src/app/timeline/page.jsx
+++ src/app/timeline/page.jsx
@@ -9,13 +9,13 @@ const TimeLinePage = () => {
   const filteredTimeLine =
     filter === "all"
       ? timeLine
-      : timeLine.filter(
-          (item) => item.action.toLowerCase() == filter.toLowerCase(),
-        );
+      : timeLine.filter((item) => item.action === filter);
   console.log(filteredTimeLine, "filteredTimeLine");
   return (
     <div className="container mx-auto h-full md:min-h-screen p-2 md:p-0 my-5 md:my-15 ">
-      <h1 className="text-2xl md:text-3xl  lg:text-[48px] text-[#1F2937] mb-4 font-bold">Timeline</h1>
+      <h1 className="text-2xl md:text-3xl  lg:text-[48px] text-[#1F2937] mb-4 font-bold">
+        Timeline
+      </h1>
       <div
         className={`mt-10 mb-5 ${timeLine.length === 0 ? "hidden" : "flex"}`}
       >
@@ -36,26 +36,27 @@ const TimeLinePage = () => {
             <p className="md:text-4xl font-bold text-black">No activity yet</p>
           </div>
         ) : (
-          <div className="flex flex-col gap-5 mt-5">
+          <div className="flex flex-col gap-3 md:gap-5 md:mt-5">
             {filteredTimeLine.map((item) => (
               <div
-                key={item.id}
+                key={`${item.id}-${new Date(item.date).getTime()}`}
                 className="p-4 shadow-sm bg-white rounded-2xl flex gap-5"
               >
+                
                 <div className="pl-5 text-4xl flex justify-center items-center">
                   {item.icon}
                 </div>
-
                 <div className="flex flex-col gap-2">
-                  <h4 className="text-[20px] font-medium">
+                  <h4 className="text-base md:text-[20px] font-medium">
                     <span className="font-bold">
-                      {item.action?.toUpperCase()}
-                    </span>{" "}
-                    with &nbsp;
+                      {item.action.charAt(0)?.toUpperCase() +
+                        item.action.slice(1)}
+                    </span>
+                    &nbsp; with &nbsp;
                     {item.name}
                   </h4>
 
-                  <p className="font-semibold">
+                  <p className="font-semibold text-xs md:text-sm">
                     {new Date(item.date).toLocaleDateString("en-US", {
                       year: "numeric",
                       month: "long",
--- src/component/Friends/DetailsPage/Handle/TimeLIneHandler.jsx
+++ src/component/Friends/DetailsPage/Handle/TimeLIneHandler.jsx
@@ -1,39 +1,49 @@
 "use client";
-import BannerStats from "@/component/Banner/Stats";
 import React, { useContext } from "react";
 import { TimeLineContext } from "@/context/timeLineContext";
 import { toast } from "react-toastify";
+import { usePathname } from "next/navigation";
 
 const TimeLineHandler = ({ friend, icon, text, bg }) => {
   const { timeLine, setTimeLine } = useContext(TimeLineContext);
+  const pathName = usePathname();
 
   const handleTimeLine = () => {
+    const action = text.toLowerCase();
+
     const alreadyExisted = timeLine.find(
-      (item) => item.id === friend.id && item.action === text.toLowerCase(),
+      (item) => item.id === friend.id && item.action === action,
     );
 
     if (alreadyExisted) {
-      toast.error(`${text}already exists in timeline!`);
+      toast.error(`${text} already exists in timeline!`);
       return;
     }
 
-    setTimeLine([
-      ...timeLine,
-      {
-        ...friend,
-        action: text.toLowerCase(),
-        icon: icon,
-        date: new Date().toISOString(),
-      },
-    ]);
+   const newEntry = {
+     ...friend,
+     action,
+     icon,
+     date: new Date(),
+   };
+
+
+    setTimeLine([...timeLine, newEntry]);
+
     toast.success(`${text} added to timeline!`);
   };
 
   return (
-    <div>
-      <button onClick={handleTimeLine}>
-        <BannerStats value={icon} title={text} bg={bg} />
-      </button>
+    <div
+      onClick={handleTimeLine}
+      className={`card ${bg ? "bg-gray-100  px-30 active:scale-101" : "bg-base-100"} shadow-sm p-10 rounded-2xl flex flex-col items-center gap-3
+      ${pathName === "/" ? "hover:scale-105 transition-transform duration-300 ease-in" : ""}`}
+    >
+      <p className="stat-value text-[#244D3F] font-medium text-3xl ">{text}</p>
+
+      <div className="stat-title text-[#64748b] font-semibold text-3xl">
+        {icon}
+      </div>
     </div>
   );
 };
